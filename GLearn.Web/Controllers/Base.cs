791f0973823b37de63994a4d13c71e17423d616a
fallen gaven <gavenf2023@outlook.com>
2024-01-31 09:25:11 +0800

    feat: :sparkles: controller最小配置

---
+++ GLearn.Web/Controllers/Base.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GLearn.Web.Controllers;
+
+public class Base : ControllerBase
+{
+	public Results<Ok<string>, BadRequest> Index()
+	{
+		return TypedResults.Ok("SUCCESS");
+	}
+}
--- GLearn.Web/Program.cs
+++ GLearn.Web/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHsts();
+
+app.MapDefaultControllerRoute();
 
 app.Run();
