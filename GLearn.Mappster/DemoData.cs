1c2221c1d972e0b9733838228c4065a68307fc58
fallen gaven <gavenf2023@outlook.com>
2024-02-10 00:15:55 +0800

    feat: :sparkles: 尝试使用Mapster生产Dto
    似乎Mapster Tool 8.4.0 还不支持net8.0的目标框架

---
+++ GLearn.Mappster/DemoData.cs
@@ -0,0 +1,11 @@
+﻿using Mapster;
+
+namespace GLearn.Mappster;
+[AdaptTo("[name]Dto"), GenerateMapper]
+public class DemoData
+{
+	public string Data1 { get; set; } = string.Empty;
+	public string Data2 { get; set; } = string.Empty;
+	public string Data3 { get; set; } = string.Empty;
+	public string Data4 { get; set; } = string.Empty;
+}	
